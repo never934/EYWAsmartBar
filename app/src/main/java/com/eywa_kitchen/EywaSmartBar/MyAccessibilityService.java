650b25eeb40e8f61a27139e0c37cde96a1b92cf5
never934 <never_934@bk.ru>
2019-06-25 15:08:32 +0300

    Bar

--- app/src/main/java/com/eywa_kitchen/EywaSmartBar/MyAccessibilityService.java
+++ app/src/main/java/com/eywa_kitchen/EywaSmartBar/MyAccessibilityService.java
@@ -2,40 +2,67 @@ package com.eywa_kitchen.EywaSmartBar;
 
 import android.accessibilityservice.AccessibilityService;
 import android.accessibilityservice.GestureDescription;
+import android.content.Context;
+import android.content.Intent;
 import android.graphics.Path;
 import android.graphics.PixelFormat;
+import android.net.Uri;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.constraint.ConstraintLayout;
+import android.support.design.widget.BottomSheetBehavior;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
 import android.view.accessibility.AccessibilityEvent;
 import android.widget.FrameLayout;
 
+import static android.support.design.widget.BottomSheetBehavior.STATE_DRAGGING;
+import static android.support.design.widget.BottomSheetBehavior.STATE_EXPANDED;
+import static android.support.design.widget.BottomSheetBehavior.STATE_HIDDEN;
+import static android.support.design.widget.BottomSheetBehavior.STATE_SETTLING;
+
 public class MyAccessibilityService extends AccessibilityService {
 
-    float upX;
-    float downX;
+
+
+    private WindowManager windowManager;
+    private WindowManager BarManager;
+    private WindowManager.LayoutParams params;
+    private WindowManager.LayoutParams BarParams;
+    private FrameLayout rootView;
+    private View llBottomSheet;
+    private BottomSheetBehavior HomeBar;
 
     @Override
     protected void onServiceConnected() {
-        WindowManager windowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
-        FrameLayout layout = new FrameLayout(this);
+        LineState();
+    }
 
-        WindowManager.LayoutParams params = new WindowManager.LayoutParams(WindowManager.LayoutParams.MATCH_PARENT,
-                WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.TYPE_ACCESSIBILITY_OVERLAY,
-                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE| WindowManager.LayoutParams.FLAG_FULLSCREEN |
-                        WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE|
+    private void LineState(){
+        windowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
+        rootView = new FrameLayout(this);
+
+        params = new WindowManager.LayoutParams(WindowManager.LayoutParams.MATCH_PARENT,
+                5, WindowManager.LayoutParams.TYPE_ACCESSIBILITY_OVERLAY,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN |
+                        WindowManager.LayoutParams.FLAG_SPLIT_TOUCH|
+                        WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE|
                         WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS|
                         WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH,
                 PixelFormat.TRANSLUCENT);
-        params.gravity = Gravity.TOP;
+        params.gravity = Gravity.BOTTOM;
 
-        windowManager.addView(layout, params);
-        layout.setOnTouchListener(new OnSwipeTouchListener(this) {
+        windowManager.addView(rootView, params);
+        rootView.setOnTouchListener(new OnSwipeTouchListener(this) {
             public void onSwipeTop() {
                 Log.e("SYSTEM ", "TO TOP");
+                windowManager.removeView(rootView);
+                openBar();
             }
             public void onSwipeRight() {
                 Log.e("SYSTEM ", "TO RIGHT");
@@ -50,13 +77,70 @@ public class MyAccessibilityService extends AccessibilityService {
         });
     }
 
-    private void swipeToRight(){
-        Log.e("SYSTEM ", "TO RIGHT");
+    private void openBar(){
+        windowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
+        rootView = (FrameLayout) LayoutInflater.from(this).inflate(R.layout.act_beh, null);
+
+        llBottomSheet = rootView.findViewById(R.id.Homebar);
+        HomeBar = BottomSheetBehavior.from(llBottomSheet);
+
+        params = new WindowManager.LayoutParams(WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.TYPE_ACCESSIBILITY_OVERLAY,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN |
+                        WindowManager.LayoutParams.FLAG_SPLIT_TOUCH|
+                        WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE|
+                        WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS|
+                        WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH,
+                PixelFormat.TRANSLUCENT);
+
+        // Задаем позиции для нашего Layout
+        params.gravity = Gravity.BOTTOM;
+        params.x = 0;
+        params.y = 0;
+        // Отображаем наш Layout
+        windowManager.addView(rootView, params);
+
+        HomeBar.setState(STATE_HIDDEN);
+        HomeBar.setState(STATE_EXPANDED);
+
+        HomeBar.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+            @Override
+            public void onStateChanged(@NonNull View bottomSheet, int newState) {
+                if (newState == BottomSheetBehavior.STATE_DRAGGING) {
+                    HomeBar.setState(BottomSheetBehavior.STATE_EXPANDED);
+                }
+                String st = String.valueOf(newState);
+                Log.e("AAA ", st);
+            }
+
+            @Override
+            public void onSlide(@NonNull View bottomSheet, float slideOffset) {
+            }
+
+        });
+
+
+        llBottomSheet.setOnTouchListener(new OnSwipeTouchListener(this) {
+            public void onSwipeBottom() {
+                Log.e("SYSTEM ", "TO BOTTOM");
+                closeBar();
+            }
+        });
     }
 
-    private void swipeToLeft(){
-        Log.e("SYSTEM ", "TO LEFT");
+    private void closeBar(){
+        HomeBar.setState(STATE_HIDDEN);
+        windowManager.removeViewImmediate(rootView);
+        LineState();
     }
+
+
+
+
+
+
+
+
+
 
     @Override
     public void onAccessibilityEvent(AccessibilityEvent event) {
@@ -65,11 +149,5 @@ public class MyAccessibilityService extends AccessibilityService {
     @Override
     public void onInterrupt() {
 
-    }
-
-    @Override
-    protected boolean onGesture(int gestureId) {
-        Log.e("GESTURE", "called");
-        return super.onGesture(gestureId);
     }
 }
--- app/src/main/java/com/eywa_kitchen/EywaSmartBar/UIbarService.java
+++ app/src/main/java/com/eywa_kitchen/EywaSmartBar/UIbarService.java
@@ -31,7 +31,11 @@ public class UIbarService extends Service {
         Log.e("Service","STARTED");
       //  initScreenUtils();
         SetLayoutInService();
-        SetListener();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
     }
 
     @Override
@@ -45,8 +49,8 @@ public class UIbarService extends Service {
     private void SetLayoutInService(){
         manager = (WindowManager) getSystemService(WINDOW_SERVICE);
         params = new WindowManager.LayoutParams(
-                Width, // Ширина экрана
-                Height, // Высота экрана
+                WindowManager.LayoutParams.MATCH_PARENT, // Ширина экрана
+                WindowManager.LayoutParams.MATCH_PARENT, // Высота экрана
                 WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY,  // Говорим, что приложение будет поверх других. В поздних API > 26, данный флаг перенесен на TYPE_APPLICATION_OVERLAY
                 WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,// Необходимо для того чтобы TouchEvent'ы в пустой области передавались на другие приложения
                 PixelFormat.TRANSLUCENT); // Само окно прозрачное
@@ -58,29 +62,8 @@ public class UIbarService extends Service {
 
         // Отображаем наш Layout
         rootView = (LinearLayout) LayoutInflater.from(this).inflate(R.layout.bottom_sheet, null);
-        AppGrab = rootView.findViewById(R.id.grab);
         manager.addView(rootView, params);
 
-    }
-
-    private void SetListener(){
-        AppGrab.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View view, MotionEvent motionEvent) {
-
-                switch (motionEvent.getAction()){
-                    case MotionEvent.ACTION_DOWN:
-                        return true;
-                    case MotionEvent.ACTION_MOVE:
-                        params.height = Math.max(-((int) motionEvent.getRawY()), ScreenUtils.convertDpToPx(UIbarService.this, 5));
-                        manager.updateViewLayout(rootView, params);
-                        return true;
-                    case MotionEvent.ACTION_UP:
-                        return true;
-                }
-                return true;
-            }
-        });
     }
 
 }
